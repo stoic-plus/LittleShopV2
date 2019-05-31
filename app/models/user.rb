0e28569dc572ff254827a295b325c745f6f69442
stoic-plus <ricardoledesmadev@gmail.com>
2019-05-31 10:35:41 -0600

    adds secure password to user and user routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
   has_many :items
   has_many :orders
 end
--- db/migrate/20190519153329_create_users.rb
+++ db/migrate/20190519153329_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :password_digest
       t.integer :role
-      t.boolean :active
+      t.boolean :active, default: true
       t.string :name
       t.string :address
       t.string :city
