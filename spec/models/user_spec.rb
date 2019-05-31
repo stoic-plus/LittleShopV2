cfa6e83252cfae69e454feb0a3be04ca32e002f1
stoic-plus <ricardoledesmadev@gmail.com>
2019-05-31 10:45:11 -0600

    adds testing for regular user scope

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 describe User do
-  it {should have_many :orders}
-  it {should have_many :items}
+  context 'relationships' do
+    it {should have_many :orders}
+    it {should have_many :items}
+  end
+  context 'scopes' do
+    it 'regular - returns all non admin or merchant users' do
+      u_1 = User.create(email: "wes", password: "ema", role: 0, name: "dad", address: "dads", city: "dasd", state: "CO", zip: 80221)
+      u_2 = User.create(email: "wes", password: "ema", role: 1, name: "dad", address: "dads", city: "dasd", state: "CO", zip: 80221)
+      actual = User.regular
+
+      expect(actual).to eq(u_1)
+    end
+  end
 end
