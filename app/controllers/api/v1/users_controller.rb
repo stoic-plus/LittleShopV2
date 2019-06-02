27b89761ffb6d93b5d67e4b4e3f5b43b9479c098
stoic-plus <ricardoledesmadev@gmail.com>
2019-06-02 17:00:14 -0600

    refactors getting users based on user_type param

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,14 @@
 class Api::V1::UsersController < ApplicationController
   def index
-    render json: UserSerializer.new(User.all), status: 200
+    render json: UserSerializer.new(get_users(params["user_type"])), status: 200
+  end
+
+  private
+
+  def get_users(type)
+    return User.all unless type
+    return User.regular if type == "regular"
+    return User.merchant if type == "merchant"
+    return User.admin if type == "admin"
   end
 end
--- spec/requests/get_users_spec.rb
+++ spec/requests/get_users_spec.rb
@@ -16,7 +16,7 @@ describe 'GET /users', type: :request do
       expect(response.status).to eq(200)
       actual = JSON.parse(response.body)["data"]
       expected = User.all
-      expect(actual.size).to eq(3)
+      expect(actual.size).to eq(4)
       actual.each_with_index do |user, i|
         expect(actual[i]["id"].to_i).to eq(expected[i]["id"])
         expect(actual[i]["attributes"]["email"]).to eq(expected[i]["email"])
@@ -32,32 +32,32 @@ describe 'GET /users', type: :request do
     after :each do
       expect(response).to be_successful
       expect(response.status).to eq(200)
-      actual.each_with_index do |user, i|
-        expect(actual[i]["id"].to_i).to eq(expected[i]["id"])
-        expect(actual[i]["attributes"]["email"]).to eq(expected[i]["email"])
-        expect(actual[i]["attributes"]["password"]).to eq(expected[i]["password"])
-        expect(actual[i]["attributes"]["name"]).to eq(expected[i]["name"])
-        expect(actual[i]["attributes"]["city"]).to eq(expected[i]["city"])
-        expect(actual[i]["attributes"]["zip"]).to eq(expected[i]["zip"])
+      @actual.each_with_index do |user, i|
+        expect(@actual[i]["id"].to_i).to eq(@expected[i]["id"])
+        expect(@actual[i]["attributes"]["email"]).to eq(@expected[i]["email"])
+        expect(@actual[i]["attributes"]["password"]).to eq(@expected[i]["password"])
+        expect(@actual[i]["attributes"]["name"]).to eq(@expected[i]["name"])
+        expect(@actual[i]["attributes"]["city"]).to eq(@expected[i]["city"])
+        expect(@actual[i]["attributes"]["zip"]).to eq(@expected[i]["zip"])
       end
     end
     it "set to regular returns those users only" do
       get '/api/v1/users?user_type=regular'
-      expected = User.regular
-      actual = JSON.parse(response.body)["data"]
-      expect(actual.size).to eq(2)
+      @expected = User.regular
+      @actual = JSON.parse(response.body)["data"]
+      expect(@actual.size).to eq(2)
     end
     it 'set to admin returns those users only' do
       get '/api/v1/users?user_type=admin'
-      expected = User.admin
-      actual = JSON.parse(response.body)["data"]
-      expect(actual.size).to eq(1)
+      @expected = User.admin
+      @actual = JSON.parse(response.body)["data"]
+      expect(@actual.size).to eq(1)
     end
     it 'set to merchant returns those users only' do
       get '/api/v1/users?user_type=merchant'
-      expected = User.merchant
-      actual = JSON.parse(response.body)["data"]
-      expect(actual.size).to eq(1)
+      @expected = User.merchant
+      @actual = JSON.parse(response.body)["data"]
+      expect(@actual.size).to eq(1)
     end
   end
 end
