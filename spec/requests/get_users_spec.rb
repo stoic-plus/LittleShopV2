79c3600b2d6bf6a0fd840740b5ce546b21140325
stoic-plus <ricardoledesmadev@gmail.com>
2019-06-02 16:44:59 -0600

    Adds testing for all user roles

--- spec/requests/get_users_spec.rb
+++ spec/requests/get_users_spec.rb
@@ -4,32 +4,60 @@ describe 'GET /users', type: :request do
   before :each do
     @u_1 = User.create(email: "wes", password: "ema", role: 0, name: "dad", address: "dads", city: "dasd", state: "CO", zip: 80221)
     @u_2 = User.create(email: "wed", password: "ema", role: 0, name: "dad", address: "dads", city: "dasd", state: "CO", zip: 80221)
-    @u_3 = User.create(email: "wea", password: "ema", role: 1, name: "dad", address: "dads", city: "dasd", state: "CO", zip: 80221)
+    @u_3 = User.create(email: "wea", password: "elma", role: 1, name: "dad", address: "dads", city: "dasd", state: "CO", zip: 80221)
+    @u_4 = User.create(email: "wea", password: "elma", role: 2, name: "dad", address: "dads", city: "dasd", state: "CO", zip: 80221)
   end
 
   context 'base route' do
-    it " returns all users with no parameters" do
+    it "returns all users with no parameters" do
       get '/api/v1/users'
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
       actual = JSON.parse(response.body)["data"]
-      expected = User.regular
-
+      expected = User.all
       expect(actual.size).to eq(3)
       actual.each_with_index do |user, i|
-        binding.pry
         expect(actual[i]["id"].to_i).to eq(expected[i]["id"])
-        expect(actual[i]["email"]).to eq(expected[i]["email"])
-        expect(actual[i]["password"]).to eq(expected[i]["password"])
-        expect(actual[i]["name"]).to eq(expected[i]["name"])
-        expect(actual[i]["city"]).to eq(expected[i]["city"])
-        expect(actual[i]["zip"]).to eq(expected[i]["zip"])
+        expect(actual[i]["attributes"]["email"]).to eq(expected[i]["email"])
+        expect(actual[i]["attributes"]["password"]).to eq(expected[i]["password"])
+        expect(actual[i]["attributes"]["name"]).to eq(expected[i]["name"])
+        expect(actual[i]["attributes"]["city"]).to eq(expected[i]["city"])
+        expect(actual[i]["attributes"]["zip"]).to eq(expected[i]["zip"])
       end
     end
   end
 
-  context '/admin returns admin users only' do
-
+  context 'user_type parameter' do
+    after :each do
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+      actual.each_with_index do |user, i|
+        expect(actual[i]["id"].to_i).to eq(expected[i]["id"])
+        expect(actual[i]["attributes"]["email"]).to eq(expected[i]["email"])
+        expect(actual[i]["attributes"]["password"]).to eq(expected[i]["password"])
+        expect(actual[i]["attributes"]["name"]).to eq(expected[i]["name"])
+        expect(actual[i]["attributes"]["city"]).to eq(expected[i]["city"])
+        expect(actual[i]["attributes"]["zip"]).to eq(expected[i]["zip"])
+      end
+    end
+    it "set to regular returns those users only" do
+      get '/api/v1/users?user_type=regular'
+      expected = User.regular
+      actual = JSON.parse(response.body)["data"]
+      expect(actual.size).to eq(2)
+    end
+    it 'set to admin returns those users only' do
+      get '/api/v1/users?user_type=admin'
+      expected = User.admin
+      actual = JSON.parse(response.body)["data"]
+      expect(actual.size).to eq(1)
+    end
+    it 'set to merchant returns those users only' do
+      get '/api/v1/users?user_type=merchant'
+      expected = User.merchant
+      actual = JSON.parse(response.body)["data"]
+      expect(actual.size).to eq(1)
+    end
   end
 end
